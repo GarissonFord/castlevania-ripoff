1355dd37ec1f08af7e113f7b56fe9744a9d7f338
Gary DiCiano <garydiciano4@gmail.com>
2018-12-07 16:35:44 -0500

    Spawn Points
    Introduced spawn points. Now the behaviors of the enemies need to be cleaned up, included their exact Y-coordinate when spawning as well as their movement patterns (especially the ghosts).

---
+++ castlevania-ripoff/Assets/Scripts/SpawnPoint.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPoint : MonoBehaviour {
+
+    public GameObject enemyToSpawn;
+    public bool playerInRange;
+
+    public float timeSinceLastSpawn, timeBetweenSpawns;
+
+    public void Update()
+    {
+        //If player is close enough
+        if (playerInRange)
+        {
+            //Start spawning enemies incrementally
+            if (Time.time - timeSinceLastSpawn >= timeBetweenSpawns)
+            {
+                Instantiate(enemyToSpawn, new Vector3(transform.position.x, -0.05f, 0.0f), gameObject.transform.rotation);
+                timeSinceLastSpawn = Time.time;
+            }
+        }
+    }
+    
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        playerInRange = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        playerInRange = false;
+    }
+}
